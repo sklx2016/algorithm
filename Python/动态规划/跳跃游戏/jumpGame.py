f7914518f67b6cf8d6b3fa413e4d12b7e57b79cc
宋凯 <songkai@qiyi.com>
2021-07-23 17:52:56 +0800

    1.跳跃游戏 2.数组最长连续1的个数

--- Python/动态规划/跳跃游戏/jumpGame.py
+++ Python/动态规划/跳跃游戏/jumpGame.py
@@ -1,3 +1,30 @@
+'''
+leetcode 55
+给定一个非负整数数组，你最初位于数组的第一个位置。
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个位置。
+'''
+
+# DP
+def jumpGameDp(array):
+    n = len(array)
+    dp = [0] * n
+    dp[0] = 1
+    for i in range(n):
+        if dp[i] != 1 : return False
+        if i + array[i] >= n - 1 : return True
+        for j in range(i + 1, i + array[i] + 1):
+            dp[j] = 1
+
+# Greedy
+def jumpGameGreedy(array):
+    n = len(array)
+    maxLen = 0
+    for i in range(n):
+        if i > maxLen : return False
+        maxLen = max(maxLen, i + array[i])
+    return maxLen >= n - 1
+
 def jumpGame(array):
     if not array:
         return 0
---
+++ Python/滑动窗口/longestOnes.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+'''=================================================
+@Author ：songkai
+@Date   ：2021/7/23 17:51
+@problem : 给定一个由若干 0 和 1 组成的数组 A ，我们最多可以将 K 个值从 0 变成 1 。
+返回仅包含 1 的最长（连续）子数组的长度。
+=================================================='''
+
+def longestOnes(array, K):
+    l, r, cnt0, res = 0, 0, 0, 0
+    while l < r :
+        if not array[r]:
+            cnt0 += 1
+            while cnt0 > K and l < r:
+                if not array[l]:
+                    l += 1
+                    cnt0 -= 1
+        res = max(res, r - l + 1)
+        r += 1
+    return res
+
