dd9c2459ad07b761bfad51dcc59d7bc220056402
宋凯 <songkai@qiyi.com>
2021-08-10 18:17:16 +0800

    二叉树最大路径和

--- Python/树/二叉树最大路径和/max_path.py
+++ Python/树/二叉树最大路径和/max_path.py
@@ -29,3 +29,29 @@ res = -100000
 
 print(max_path(tree))
 print(res)
+
+
+'''
+给定一个非空二叉树，返回其最大路径和。
+本题中，路径被定义为一条从树中任意节点出发，达到任意节点的序列。该路径至少包含一个节点，且不一定经过根节点。
+'''
+import sys
+
+class Solution:
+
+    res = -sys.maxsize
+    def maxPathSum(self, root):
+        return self.dfs(root)
+
+    def dfs(self, node):
+        if node == None:
+            return 0
+        left_max_sum = self.dfs(node.left)
+        right_max_sum = self.dfs(node.right)
+        sum_path = node.val
+        sum_path = max(sum_path, sum_path + left_max_sum)
+        sum_path = max(sum_path, sum_path + right_max_sum)
+        self.res = max(self.res, sum_path)
+        return max(0, left_max_sum, right_max_sum) + node.val
+
+
